02b04c43346ab8fdc3a617fb601965ed9e20cf6e
MPLEKON <giaxri9@gmail.com>
2025-04-10 22:21:22 +0300

    Added option to download files in v2 and v3

--- v3/sphere.js
+++
@@ -1,5 +0,0 @@
-export function createSphere(radius = 1, color = 0x00aaff) {
-    const geometry = new THREE.SphereGeometry(radius, 32, 32);
-    const material = new THREE.MeshStandardMaterial({ color });
-    return new THREE.Mesh(geometry, material);
-    }
